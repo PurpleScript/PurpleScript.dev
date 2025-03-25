63a0ecef9946a941ff815fe15e071e5f1ebdf7bd
Shahar Zidon <shaharz97@gmail.com>
2025-03-25 10:22:46 +0100

    chore - meta data

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,34 @@ export const metadata = {
     shortcut: "/images/logo.svg",
     apple: "/images/logo.svg",
   },
+  openGraph: {
+    title: "PurpleByte - Custom Technology Solutions",
+    description:
+      "We help small and medium businesses grow and operate more efficiently through custom technology solutions.",
+    url: "https://purplebyte.com",
+    siteName: "PurpleByte",
+    images: [
+      {
+        url: "/images/logo.svg",
+        width: 300,
+        height: 300,
+        alt: "PurpleByte Logo",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "PurpleByte - Custom Technology Solutions",
+    description:
+      "We help small and medium businesses grow and operate more efficiently through custom technology solutions.",
+    images: ["/images/logo.svg"],
+  },
+  other: {
+    "og:image:width": "300",
+    "og:image:height": "300",
+  },
 };
 
 export default function RootLayout({
